d6158ee0fe571fe3247afe94acbcba293972d66f
rdhmuhammad <rdhmuhammad57@gmail.com>
2023-09-13 09:16:47 +0700

    fix: apply new rule to dto

--- dto/dto.go
+++ dto/dto.go
@@ -3,9 +3,15 @@ package dto
 type NotaBody struct {
 	TransactionID     uint                  `json:"transactionId"`
 	NotaBranchDetail  NotaBranchDetail      `json:"notaBranchDetail"`
-	TransactionDetail NotaTransactionDetail `pdfField:"type:tablePivot;tableRowHeight:6;dy:-20" json:"transactionDetail"`
+	TransactionDetail NotaTransactionDetail `pdfField:"type:tablePivot;tableRowHeight:6;usingColon:true;colWidths:35,5,60" json:"transactionDetail"`
+	Divider           string                `pdfField:"type:text" json:"-"`
+	CustomerName      string                `pdfField:"type:text;fontSize:8" json:"customerName"`
 	ServiceDetail     NotaServiceDetail     `json:"service"`
-	Payment           NotaPayment           `json:"payment"`
+	Divider1          string                `pdfField:"type:text" json:"-"`
+
+	Payment  NotaPayment `pdfField:"type:tablePivot;tableRowHeight:6;dx:39;colWidths:30,70" json:"payment"`
+	Divider2 string      `pdfField:"type:text" json:"-"`
+	FootNote string      `pdfField:"type:text" json:"footNote"`
 }
 
 type NotaBranchDetail struct {
@@ -17,32 +23,37 @@ type NotaBranchDetail struct {
 }
 
 type NotaServiceDetail struct {
-	Services   []NotaService `pdfField:"type:table;tableRowHeight:6;dy:-18" json:"services"`
-	TotalPrice uint          `pdfField:"type:text;name:totalPrice;dy:55;dx:35"  json:"totalPrice"`
+	Services   []NotaService `pdfField:"type:table;tableRowHeight:6" json:"services"`
+	TotalPrice uint          `json:"totalPrice"`
 }
 
 type NotaService struct {
-	Name       string `pdfField:"colName:Name" json:"name"`
-	Quantity   uint   `pdfField:"colName:Quantity" json:"quantity"`
-	UnitAmount string `pdfField:"colName:Unit Amount" json:"unitAmount"`
-	Units      string `json:"units"`
-	Price      uint   `pdfField:"colName:Price" json:"price"`
+	Name              string `pdfField:"colName:Name" json:"name"`
+	QuantityFormatted string `pdfField:"colName:Quantity" json:"quantityFormatted"`
+	Quantity          uint   `json:"quantity"`
+	UnitAmount        string `json:"unitAmount"`
+	Units             string `json:"units"`
+	PriceFormatted    string `pdfField:"colName:Price" json:"priceFormatted"`
+	Price             uint   `json:"price"`
 }
 
 type NotaPayment struct {
-	Status string `json:"status"`
-	Method string `json:"method"`
-	PaidAt string `json:"paidAt"`
-	Paid   uint   `json:"paid"`
-	Remain uint   `json:"remain"`
+	Status              string `json:"status"`
+	Method              string `json:"method"`
+	TotalPriceFormatted string `pdfField:"colName:Total Harga" json:"totalPriceFormatted"`
+	TotalPrice          uint   `json:"totalPrice"`
+	PaidFormatted       string `pdfField:"colName:Terbayar" json:"paidFormatted"`
+	Paid                uint   `json:"paid"`
+	RemainFormatted     string `pdfField:"colName:Sisa" json:"remainFormatted"`
+	Remain              uint   `json:"remain"`
 }
 
 type NotaTransactionDetail struct {
-	ReferenceNumber string `pdfField:"colName:ID Referensi" json:"referenceNumber"`
+	ReferenceNumber string `pdfField:"colName:Nomor Nota" json:"referenceNumber"`
 	NotaPdf         string `json:"notaFile"`
 	NotaWa          string `json:"whatsappCTA"`
-	Name            string `pdfField:"colName:Nama Pelanggan" json:"name"`
 	Phone           string `pdfField:"colName:Nomor Wa" json:"phone"`
-	StartedAt       string `pdfField:"colName:Dimulai dari" json:"startedAt"`
+	StartedAt       string `pdfField:"colName:Laundri Masuk" json:"startedAt"`
 	FinsihedAt      string `pdfField:"colName:Estimasi" json:"finishedAt"`
+	PaidAt          string `pdfField:"colName:Dibayar Pada" json:"paidAt"`
 }
